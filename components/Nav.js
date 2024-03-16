9f8201e9bbb7584da64667addc43aaebe86db5c5
Chin Seila <121209834+chinseila168@users.noreply.github.com>
2024-03-16 16:03:21 +0700

    first commit

--- components/Nav.js
+++ components/Nav.js
@@ -36,11 +36,12 @@ const Nav = () => {
   const router = userRouter();
   const pathname = router.pathname;
   return (
-    <nav className='flex flex-col items-center xl:justify-center'>
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:max-w-md xl:h-screen">
       {/* inner */}
-      <div className="flex w-full xl:flex-col item-center justify-between xl:justify-center gap-y-10 px-4 m:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full ">
+      <div className="flex w-full xl:flex-col item-center justify-between xl:justify-center gap-y-10 px-4 m:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full ">
         {navData.map((Link, index) => {
-          return <Link href={link.path}>{link.icon}</Link>;
+          return <Link href={link.path} key={index}>
+            {link.icon}</Link>;
         })}
       </div>
     </nav>
