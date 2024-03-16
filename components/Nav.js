fc48052ef7e66e195067b8005dfaef20e342a0f0
Chin Seila <121209834+chinseila168@users.noreply.github.com>
2024-03-16 15:54:32 +0700

    first commit

--- components/Nav.js
+++ components/Nav.js
@@ -6,28 +6,45 @@ import {
   HiRectangleGroup,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from 'react-icons/hi2';
+} from "react-icons/hi2";
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
+// next link
+import Link from "next/link";
+
+// next router
+import { userRouter } from "next/router";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = userRouter();
+  const pathname = router.pathname;
+  return (
+    <nav className='flex flex-col items-center xl:justify-center'>
+      {/* inner */}
+      <div className="flex w-full xl:flex-col item-center justify-between xl:justify-center gap-y-10 px-4 m:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full ">
+        {navData.map((Link, index) => {
+          return <Link href={link.path}>{link.icon}</Link>;
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
