8a1ca2f319bff235eda5104862c02000d2a99da8
Chin Seila <chinseila008@gmail.com>
2024-04-26 20:37:21 +0700

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1350027cf43e191f"; }
+/******/ 		__webpack_require__.h = function() { return "e0387176621126f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
